16b88265cf6c2adbc3a699bc04937b2264f20ff7
yuxuanli <yuxuanli@digitalgd.com.cn>
2023-09-21 15:58:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Monaco ref="monacoEdit" v-model="text" :readonly="false" type='sql' class="monaco" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent, ref } from 'vue'
+import Monaco from "./components/monaco/_base.vue"
+const text = ref<string>('');
+</script>
+
+<style></style>
